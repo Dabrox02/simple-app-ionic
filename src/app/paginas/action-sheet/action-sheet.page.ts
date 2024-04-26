f0c70c1827d2ec74f2ce49684da6a84f10447f73
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-04-25 19:44:59 -0500

    feat: :sparkles: action sheet component

--- src/app/paginas/action-sheet/action-sheet.page.ts
+++ src/app/paginas/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonActionSheetCustomEvent,OverlayEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-action-sheet',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./action-sheet.page.scss'],
 })
 export class ActionSheetPage implements OnInit {
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  logResult($event: IonActionSheetCustomEvent<OverlayEventDetail<any>>) {
+    console.log(JSON.stringify($event.detail, null, 2));
   }
 
 }
