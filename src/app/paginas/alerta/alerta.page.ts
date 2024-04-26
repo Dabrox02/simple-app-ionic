7c060cbcad01101d927c3347bcf1f0342f6b0187
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-04-25 20:14:34 -0500

    feat: :sparkles: buttons types and properties

--- src/app/paginas/alerta/alerta.page.ts
+++ src/app/paginas/alerta/alerta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonAlertCustomEvent,OverlayEventDetail } from '@ionic/core';
 import { EncabezadoComponent } from 'src/app/componentes/encabezado/encabezado.component';
 
 @Component({
@@ -8,11 +9,22 @@ import { EncabezadoComponent } from 'src/app/componentes/encabezado/encabezado.c
 })
 export class AlertaPage implements OnInit {
 
+  alertButtons = ['Ok', {
+    text: 'cancel',
+    role: 'cancel',
+    handler: () => {
+      console.log('Alerta cancelada');
+    },
+  }];
   initialComponent:string = "/inicio";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setResult($event: IonAlertCustomEvent<OverlayEventDetail<any>>) {
+    console.log(`Dismissed with role: ${$event.detail.role}`);
   }
 
 }
